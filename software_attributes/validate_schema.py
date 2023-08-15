0ee61d15e28bda65cc34241b074e5751dc99b8f6
Rafael M Mudafort <rafmudaf@gmail.com>
2023-08-15 16:26:02 -0500

    Add a GitHub Actions job to check model attributes

---
+++ software_attributes/validate_schema.py
@@ -0,0 +1,22 @@
+from jsonschema import Draft202012Validator
+from schema import schema
+import yaml
+from pathlib import Path
+
+draft_202012_validator = Draft202012Validator(schema)
+
+if __name__=="__main__":
+
+    model_list = [
+        "floris"
+    ]
+
+    for model in model_list:
+        print(f"Validating {model}...")
+        input_file = Path("weto_software", f"{model}.yaml")
+        with open(input_file, "r") as infile:
+            input_dict = yaml.load(infile, yaml.SafeLoader)
+        validate_dict = input_dict
+
+        draft_202012_validator.validate(validate_dict)
+        print(f"done.")
