acb478f4666de0e8586b13cf2f1317f7314291a9
Rafael M Mudafort <rafmudaf@gmail.com>
2023-08-28 19:10:48 -0500

    Support a longer list of software licenses

--- software_attributes/schema.py
+++ software_attributes/schema.py
@@ -64,9 +64,46 @@ schema = {
             "enum": ["python", "fortran", "matlab", "c++", "rust", "julia", "r"]
         },
         "license_type": {
-            "description": "Choose the corresponding license string from the "
-                "[Open Source Initiative Approved Licenses](https://opensource.org/licenses/).",
-            "enum": ["Apache-2.0", "LGPL-2.1"]
+            "description": "Choose the corresponding license string from "
+                "[this list of license types](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository#searching-github-by-license-type).",
+            "enum": [
+                "afl-3.0",
+                "apache-2.0",
+                "artistic-2.0",
+                "bsl-1.0",
+                "bsd-2-clause",
+                "bsd-3-clause",
+                "bsd-3-clause-clear",
+                "bsd-4-clause",
+                "0bsd",
+                "cc",
+                "cc0-1.0",
+                "cc-by-4.0",
+                "cc-by-sa-4.0",
+                "wtfpl",
+                "ecl-2.0",
+                "epl-1.0",
+                "epl-2.0",
+                "eupl-1.1",
+                "agpl-3.0",
+                "gpl",
+                "gpl-2.0",
+                "gpl-3.0",
+                "lgpl",
+                "lgpl-2.1",
+                "lgpl-3.0",
+                "isc",
+                "lppl-1.3c",
+                "ms-pl",
+                "mit",
+                "mpl-2.0",
+                "osl-3.0",
+                "postgresql",
+                "ofl-1.1",
+                "ncsa",
+                "unlicense",
+                "zlib",
+            ]
         },
         "data_collection_methods": {
             "description": "\n".join([
