d057cbdb61bd89d3907fc9c551c751351f468739
Rafael M Mudafort <rafmudaf@gmail.com>
2025-01-29 22:34:37 -0600

    Toggle node font style with state

--- entry_guide/src/DecisionNode.vue
+++ entry_guide/src/DecisionNode.vue
@@ -1,11 +1,30 @@
-<script setup>
+<script setup lang="ts">
 import { useNode } from '@vue-flow/core'
 
 const { node } = useNode();
 
+const isA = (state: string) => {
+  return state === "a";
+};
+const isB = (state: string) => {
+  return state === "b"
+};
+
 </script>
 
 <template>
-  <div>{{ node.data.a }} | {{ node.data.b }}
+  <div>
+    <span :class="{'bold': isA(node.data.state)}" class="label">{{ node.data.a }}</span> |
+    <span :class="{'bold': isB(node.data.state)}" class="label">{{ node.data.b }}</span>
   </div>
 </template>
+
+<style>
+.label {
+  font-weight: normal;
+  color: "white";
+}
+.bold {
+  font-weight: bold;
+}
+</style>
