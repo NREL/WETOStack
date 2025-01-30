a8b32cfa4b432b92689674d917ec1d87fb3606e6
Rafael M Mudafort <rafmudaf@gmail.com>
2025-01-30 02:44:55 -0600

    Add tool node styling

--- entry_guide/src/ToolNode.vue
+++ entry_guide/src/ToolNode.vue
@@ -6,6 +6,26 @@ const { node } = useNode();
 </script>
 
 <template>
-  <div>{{ node.data.label }}
+  <div :class="{'reachable': node.data.reachable}" class="node">
+    <span :class="{'bold': node.data.reachable}" class="label">{{ node.data.label }}</span>
   </div>
 </template>
+
+<style scoped>
+.label {
+  font-weight: normal;
+  color: black;
+}
+.bold {
+  font-weight: bold;
+}
+.node {
+  padding: 8px;
+  border-radius: 4px;
+  text-align: center;
+  background-color: #ccc;
+}
+.reachable {
+  background-color: #28a745;
+}
+</style>
