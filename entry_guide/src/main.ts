c11809ef704eb3b6e73bb306bab2d14e42d60d5d
Rafael M Mudafort <rafmudaf@gmail.com>
2025-05-02 14:56:55 -0500

    Add GA tag to entry guide

--- entry_guide/src/main.ts
+++ entry_guide/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { configure } from "vue-gtag";
 import './style.css'
 import App from './App.vue'
 
+configure({
+    tagId: "G-XK6LTW8XM7"
+})
 createApp(App).mount('#app')
